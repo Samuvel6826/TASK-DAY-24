441bfe11b63397ab960a45fbdd90ab7d6cb8cb12
Samuvel6826 <samuvel6826@gmail.com>
2023-11-15 19:52:41 +0530

    sb-admin-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./components/Dashboard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+     <div id="wrapper">
+        <Sidebar/>
+        <div id="content-wrapper" className="d-flex flex-column">
+        <div id="content">
+            <Dashboard/>
+          </div>
+          </div>
+      </div>
+  </>
 }
 
 export default App;
